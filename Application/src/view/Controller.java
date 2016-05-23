d2dbd4f8672dd53d6d8a20fc738cf16c0d566a1d
PatrickLoka <patrick.loka@anderscore.com>
2016-05-23 19:29:17 +0200

    init VC design

--- Application/src/view/Controller.java
+++ Application/src/view/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package view;
 
 public class Controller {
 }
--- Application/src/view/Main.java
+++ Application/src/view/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
