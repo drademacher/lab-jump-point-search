e68173d6eb741e3198ff560f01b8170e9b00e401
Danny Rademacher <danny.rademacher@gmail.com>
2016-05-25 22:46:46 +0200

    initial layout

--- Application/src/view/Controller.java
+++ Application/src/view/Controller.java
@@ -1,4 +1,25 @@
 package view;
 
-public class Controller {
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML //  fx:id="myButton"
+    private Button play;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        play.setOnAction(e -> System.out.print("nice"));
+        System.out.print("check");
+
+        // initialize your logic here: all @FXML variables will have been injected
+
+    }
 }
--- Application/src/view/Main.java
+++ Application/src/view/Main.java
@@ -9,10 +9,10 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.show();
     }
 
