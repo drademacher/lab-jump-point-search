5529b894bf88e9d1fa5edc3ba1a5d10cc18b00e4
Danny Rademacher <danny.rademacher@gmail.com>
2016-05-25 23:49:19 +0200

    grid graph visualized

--- Application/src/view/Controller.java
+++ Application/src/view/Controller.java
@@ -3,23 +3,36 @@ package view;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    @FXML //  fx:id="myButton"
-    private Button play;
+    @FXML private Button playButton;
+    @FXML private Button prevButton;
+    @FXML private Button nextButton;
+    @FXML private Button resetButton;
+    @FXML private MenuItem emptyMap;
+    @FXML private MenuItem randomMap;
+    @FXML private MenuItem genMap;
+    @FXML private MenuItem loadMap;
+    @FXML public Canvas canvas;
+
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        play.setOnAction(e -> System.out.print("nice"));
-        System.out.print("check");
+        emptyMap.setOnAction(e -> System.out.println("nice"));
+        prevButton.setOnAction(e -> System.out.println("nice"));
 
-        // initialize your logic here: all @FXML variables will have been injected
+
 
     }
 }
--- Application/src/view/Main.java
+++ Application/src/view/Main.java
@@ -2,22 +2,68 @@ package view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1200, 800));
-        primaryStage.show();
-    }
-
+    private Canvas canvas;
+    private int size = 20;
+    private int n;
+    private int m;
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // load stuff
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        BorderPane p = fxmlLoader.load(getClass().getResource("main.fxml").openStream());
+        Controller ctrl = fxmlLoader.getController();
+        this.canvas = ctrl.canvas;
+
+        // initialize stage
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(p, 1200, 800));
+        primaryStage.show();
+
+        // settings
+        n = (int) (((StackPane) canvas.getParent()).getWidth() / size);
+        m = (int) (((StackPane) canvas.getParent()).getHeight() / size);
+
+
+        canvas.setWidth(size * n);
+        canvas.setHeight(size * m);
+
+        render();
+    }
+
+    private void render() {
+        // full rendering of the map
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < m; y++) {
+                // change color
+                // gc.setFill(CELLS[grid.getCell(x, y)]);
+
+                // draw rect
+                gc.setFill(Color.BLACK);
+                gc.fillRect(x * size, y * size, size, size);
+
+                gc.setFill(Color.GRAY);
+                gc.fillRect(x * size, y * size, size - 1, size - 1);
+            }
+        }
+
+        gc.clearRect(0, canvas.getHeight() - 2, canvas.getWidth(), canvas.getHeight());
+        gc.clearRect(canvas.getWidth() - 2, 0, canvas.getWidth(), canvas.getHeight());
     }
 }
