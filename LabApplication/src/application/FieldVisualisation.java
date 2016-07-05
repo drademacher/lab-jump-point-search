7c79e78bf53660190068787716f89257fa23c2c9
Danny Rademacher <danny.rademacher@gmail.com>
2016-07-05 21:30:00 +0200

    fix light grey

--- LabApplication/src/application/FieldVisualisation.java
+++ LabApplication/src/application/FieldVisualisation.java
@@ -12,7 +12,7 @@ enum FieldVisualisation {
     JUMP_POINT("#595959"),
     START_POINT("#006AD9"),
     OBSTACLE_POINT("#212121"),
-    VISITED_POINT("#D9D9D9"),
+    VISITED_POINT("#B3B3B3"),
     PATH_POINT("#2BD942");
 
     // TODO: use color #D92BD3 for fields of jump points
